1bb696ecb39d747eb825f8c269655f8b1cc2386f
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-12-12 16:38:29 +0100

    ADDED: icons non renderizzate

--- src/components/ShowFilm.jsx
+++ src/components/ShowFilm.jsx
@@ -3,15 +3,15 @@ import TvContext from "../context/TvContext";
 
 export default function ShowFilmTv() {
   const { Film } = useContext(TvContext);
-  console.log(Film);
+  console.log(`Film:`, Film);
   if (Film.length === 0) {
     return null;
   } else {
     return (
       <div>
         <h1>Film:</h1>
-        {Film.map((el) => (
-          <div key={el.id}>
+        {Film.map((el, index) => (
+          <div key={index}>
             <h3>{el.title}</h3>
             <h4>{el.original_title}</h4>
             <p>{el.original_language}</p>
--- src/components/ShowTv.jsx
+++ src/components/ShowTv.jsx
@@ -3,18 +3,19 @@ import TvContext from "../context/TvContext";
 
 export default function ShowFilmTv() {
   const { Tv } = useContext(TvContext);
-
+  console.log(`serie tv:`, Tv);
   if (Tv.length === 0) {
     return null;
   } else {
     return (
       <div>
+        <img src="../assets/icons/it.png" alt="" />
         <h1>Serie tv:</h1>
-        {Tv.map((el) => (
-          <div key={el.id}>
+        {Tv.map((el, index) => (
+          <div key={index}>
             <h3>{el.name}</h3>
             <h4>{el.original_name}</h4>
-            <p>{el.original_language}</p>
+            <img src={`../assets/icons/${el.original_language}.png`} alt="" />
             <p>{el.vote_average}</p>
           </div>
         ))}
