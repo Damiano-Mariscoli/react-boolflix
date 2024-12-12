ed14a1d7308ec806416f4bd0b5bab7a271ac867b
Damiano-Mariscoli <damiano.mariscoli2@gmail.com>
2024-12-12 15:52:09 +0100

    distinti film e serie tv

--- src/components/ShowFilm.jsx
+++ src/components/ShowFilm.jsx
@@ -4,17 +4,21 @@ import TvContext from "../context/TvContext";
 export default function ShowFilmTv() {
   const { Film } = useContext(TvContext);
   console.log(Film);
-  return (
-    <div>
-      <h1>Film:</h1>
-      {Film.map((el) => (
-        <div key={el.id}>
-          <h2>{el.title}</h2>
-          <h3>{el.original_title}</h3>
-          <p>{el.original_language}</p>
-          <p>{el.vote_average}</p>
-        </div>
-      ))}
-    </div>
-  );
+  if (Film.length === 0) {
+    return null;
+  } else {
+    return (
+      <div>
+        <h1>Film:</h1>
+        {Film.map((el) => (
+          <div key={el.id}>
+            <h3>{el.title}</h3>
+            <h4>{el.original_title}</h4>
+            <p>{el.original_language}</p>
+            <p>{el.vote_average}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
--- src/components/ShowTv.jsx
+++ src/components/ShowTv.jsx
@@ -3,18 +3,22 @@ import TvContext from "../context/TvContext";
 
 export default function ShowFilmTv() {
   const { Tv } = useContext(TvContext);
-  console.log(Tv);
-  return (
-    <div>
-      <h1>Serie tv:</h1>
-      {Tv.map((el) => (
-        <div key={el.id}>
-          <h2>{el.name}</h2>
-          <h3>{el.original_name}</h3>
-          <p>{el.original_language}</p>
-          <p>{el.vote_average}</p>
-        </div>
-      ))}
-    </div>
-  );
+
+  if (Tv.length === 0) {
+    return null;
+  } else {
+    return (
+      <div>
+        <h1>Serie tv:</h1>
+        {Tv.map((el) => (
+          <div key={el.id}>
+            <h3>{el.name}</h3>
+            <h4>{el.original_name}</h4>
+            <p>{el.original_language}</p>
+            <p>{el.vote_average}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
